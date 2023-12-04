3b1b926b46c889d79c01d3f54f197086e0a97d77
oat431 <oat431@gmail.com>
2023-12-04 20:22:49 +0700

    🛠 setup: new experiment

---
+++ src/main/java/panomete/playground/queryman/QuerymanApplication.java
@@ -0,0 +1,13 @@
+package panomete.playground.queryman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerymanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerymanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/panomete/playground/queryman/QuerymanApplicationTests.java
@@ -0,0 +1,13 @@
+package panomete.playground.queryman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerymanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
